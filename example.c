b2b3829e3b4fd13fefe0b1a4c4509456584a5b53
Tyler Russell <thebomb99534@gmail.com>
2017-02-02 21:47:40 -0600

    Using stdbool and a few safety features

--- example.c
+++ example.c
@@ -1,12 +1,16 @@
 #include "scanner.h"
 
 int main(int argc, char *argv[]) {
+	/* creating and setting up our SCANNER object */
 	SCANNER scanner;
 	scnr_Init(&scanner,"text.txt");
+
 	/* printing all the tokens */
-	while(scanner.hasNextLine)
-		printf("%s\n",scnr_nextLine(&scanner));
-	scnr_Close(&scanner);
+	while(scanner.hasNext)
+		printf("%s\n",scnr_next(&scanner));
+
+	/* cleaning up */
+	scnr_close(&scanner);
 	return 0;
 }
 
--- scanner.h
+++ scanner.h
@@ -1,69 +1,74 @@
 #ifndef SCANNER_H
 #define SCANNER_H
-#include <stdio.h> //for file input
-#include <string.h> //for C-style strings
-#include <stdlib.h> //for type conversions
 
-#define TRUE 1
-#define FALSE 0
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-typedef struct SCANNER {
+ typedef struct SCANNER {
 	FILE *file;
 	char currLine[256];
 	char currToken[32];
-	int hasNextLine; //true by default
-	int hasNext; //true by default
+	bool hasNextLine;
+	bool hasNext;
 } SCANNER;
 
 /* initializing the Scanner "object" */
-int scnr_Init(SCANNER *scanner,char *location) {
+bool scnr_Init(SCANNER *scanner,char *location) {
 	/* initializing our bools */
-	scanner->hasNext=TRUE;
-	scanner->hasNextLine=TRUE;
+	scanner->hasNext=true;
+	scanner->hasNextLine=true;
 
 	/* opening a file and doing some error checking */
 	scanner->file=fopen(location,"r");
 	if (scanner->file == NULL) {
 		printf("Could not open the file at location %s.",location);
-		return FALSE;
+		return false;
 	}
-	return TRUE;
+	return true;
 }
 
 /* returning the next token from the file */
 char* scnr_next(SCANNER *scanner) {
-	if(fscanf(scanner->file,"%s",scanner->currToken) == TRUE) //if we have another element
+	if(scanner->hasNextLine && fscanf(scanner->file,"%s",scanner->currToken) == true) //if we have another element
 		return scanner->currToken;
 	else{
-		scanner->hasNext=FALSE;
+		scanner->hasNext=false;
+		scanner->hasNextLine=false;
 		return "";
 	}
 }
 
 /* returning the next line of the file */
 char* scnr_nextLine(SCANNER *scanner) {
-	if(fscanf(scanner->file, "%[^\n]\n", scanner->currLine) != EOF) //if we have another line
+	if(scanner->hasNextLine && fscanf(scanner->file, "%[^\n]\n", scanner->currLine) != EOF) //if we have another line
 		return scanner->currLine;
 	else{
-		scanner->hasNextLine=FALSE;
+		scanner->hasNextLine=false;
+		scanner->hasNext=false;
 		return "";
 	}
 }
 
 /* returning the next token as an integer from the file */
 int scnr_nextInt(SCANNER *scanner) {
-	scnr_next(scanner);
-	return atoi(scanner->currToken);
+	if(scnr_next(scanner) == NULL)
+		return false;
+	else
+		return atoi(scanner->currToken);
 }
 
 /* returning the next token as a double from the file */
 double scnr_nextDouble(SCANNER *scanner) {
-	scnr_next(scanner);
-	return atof(scanner->currToken);
+	if(scnr_next(scanner) == NULL)
+		return false;
+	else
+		return atoi(scanner->currToken);
 }
 
 /* cleaning up */
-void scnr_Close(SCANNER *scanner) {
+void scnr_close(SCANNER *scanner) {
 	fclose(scanner->file);
 }
 #endif
