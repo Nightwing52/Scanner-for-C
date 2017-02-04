408329f2f6307a9221a980f86e961df79da2f933
Tyler Russell <Nightwing52@users.noreply.github.com>
2017-02-04 17:40:07 -0600

    Delete push.sh

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git init
-git add .
-git commit -m Fixed Naming Convention and Scanner can now be Linked in Large Projects
-git pull
-git remote add origin https://github.com/Nightwing52/Scanner-for-C.git
-git -v
-git push origin master
